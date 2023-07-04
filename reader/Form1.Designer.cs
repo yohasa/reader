3e37cf2dcf7e20bb3709a47c6de8128fba5587dc
yhs <yhsaghrounu@gmail.com>
2023-07-04 23:27:46 +0300

    Day2

--- reader/Form1.Designer.cs
+++ reader/Form1.Designer.cs
@@ -32,7 +32,9 @@
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
             this.list1 = new System.Windows.Forms.RichTextBox();
-            this.btn1 = new System.Windows.Forms.Button();
+            this.cho = new System.Windows.Forms.Button();
+            this.newFile = new System.Windows.Forms.Button();
+            this.mic_ca = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
             this.SuspendLayout();
@@ -45,25 +47,47 @@
             this.list1.TabIndex = 3;
             this.list1.Text = "";
             // 
-            // btn1
+            // cho
             // 
-            this.btn1.Location = new System.Drawing.Point(334, 354);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(94, 29);
-            this.btn1.TabIndex = 4;
-            this.btn1.Text = "button1";
-            this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            this.cho.Location = new System.Drawing.Point(314, 65);
+            this.cho.Name = "cho";
+            this.cho.Size = new System.Drawing.Size(125, 85);
+            this.cho.TabIndex = 7;
+            this.cho.Text = "choise_file && extract_name_of_mail";
+            this.cho.UseVisualStyleBackColor = true;
+            this.cho.Click += new System.EventHandler(this.cho_Click);
+            // 
+            // newFile
+            // 
+            this.newFile.Location = new System.Drawing.Point(314, 213);
+            this.newFile.Name = "newFile";
+            this.newFile.Size = new System.Drawing.Size(106, 30);
+            this.newFile.TabIndex = 8;
+            this.newFile.Text = "save_to_File";
+            this.newFile.UseVisualStyleBackColor = true;
+            this.newFile.Click += new System.EventHandler(this.newFile_Click);
+            // 
+            // mic_ca
+            // 
+            this.mic_ca.Location = new System.Drawing.Point(325, 277);
+            this.mic_ca.Name = "mic_ca";
+            this.mic_ca.Size = new System.Drawing.Size(94, 29);
+            this.mic_ca.TabIndex = 9;
+            this.mic_ca.Text = "mic & cam";
+            this.mic_ca.UseVisualStyleBackColor = true;
+            this.mic_ca.Click += new System.EventHandler(this.mic_ca_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 409);
-            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.mic_ca);
+            this.Controls.Add(this.newFile);
+            this.Controls.Add(this.cho);
             this.Controls.Add(this.list1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Extract_Mail";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
@@ -75,6 +99,8 @@
         private BindingSource bindingSource1;
         private BindingSource bindingSource2;
         private RichTextBox list1;
-        private Button btn1;
+        private Button cho;
+        private Button newFile;
+        private Button mic_ca;
     }
 }
--- reader/Form1.cs
+++ reader/Form1.cs
@@ -1,9 +1,19 @@
+using System.Diagnostics;
 using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using VisioForge.MediaFramework.ONVIF;
+using Windows.Devices.Enumeration;
+using Windows.Media.Devices;
+using Color = System.Drawing.Color;
+using MediaDevice = Windows.Media.Devices.MediaDevice;
 
 namespace reader
 {
     public partial class Form1 : Form
     {
+        int c = 4;
+            string dp = "";
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +29,92 @@ namespace reader
             string PATH = @"C:\Users\USER26\source\repos\reader\reader\source.txt";
             StreamReader stream = new StreamReader(PATH);
             string data = stream.ReadToEnd();
-            list1.Text = data;
+            list1.Text = "";
+        }
+
+        private void del_Click(object sender, EventArgs e)
+        {
+            list1.Text = "";
+        }
+
+        private void edi_Click(object sender, EventArgs e)
+        {
+            string PATH = @"C:\Users\USER26\source\repos\reader\reader\source.txt";
+            StreamReader stream = new StreamReader(PATH);
+            string data = "";
+        }
+
+        private void cho_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+            //StreamWriter stream = new StreamWriter(fileName);
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            ofd.InitialDirectory = "C:\\Users\\user\\Documents";
+            //dp = Path.GetFileName(ofd.FileName);
+            //db = Path.GetDirectoryName(ofd.FileName);
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+               fileName = ofd.FileName;
+                else return;
+                using (StreamReader sr = new StreamReader(fileName)) { 
+                    string line;
+                    string searchText = "@";
+                    StringBuilder sb = new StringBuilder();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Contains(searchText))
+                    {
+                        int a = line.IndexOf('@');
+                        line = line.Substring(0, a);
+                        sb.Append(line);
+                        sb.Append("\n");
+                       
+                    }
+                   
+                }
+
+                // Get the path of the file
+                string fileName1 = "myfile"+ c++ +".txt";
+                string directoryPath = Path.GetDirectoryName(ofd.FileName);
+                string filePath = Path.Combine(directoryPath, fileName1);
+
+                // Create a new file
+                StreamWriter sw = File.CreateText(filePath);
+
+                
+
+                // Display the lines that contain the search text in the text box
+                list1.Text = sb.ToString();
+                // Write the text to the file
+                sw.WriteLine(list1.Text);
+
+                // Close the file
+                sw.Close();
+
+                // Open the file explorer
+                Process.Start("explorer.exe", filePath);
+            }
+                }
+
+        private void newFile_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void mic_ca_Click(object sender, EventArgs e)
+        {
+            //// Get the microphone device
+            //MediaDevice microphoneDevice = DeviceInformation.FindAllAsync(DeviceClass.AudioCapture).Result[0];
+
+            //// Get the camera device
+            //MediaDevice cameraDevice = DeviceInformation.FindAllAsync(DeviceClass.VideoCapture).Result[0];
+
+            //// Enable the microphone
+            //microphoneDevice.DeviceState = MediaDeviceState.Enabled;
+
+            //// Enable the camera
+            //cameraDevice.DeviceState = MediaDeviceState.Enabled;
         }
     }
 }
+
